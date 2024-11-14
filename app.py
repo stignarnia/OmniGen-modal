a0a38d966695800a6f9afb8f45f27f11a544fa4f
stignarnia <80171209+stignarnia@users.noreply.github.com>
2024-11-14 09:44:25 +0100

    set port to 80

--- app.py
+++ app.py
@@ -427,4 +427,4 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # launch
-    demo.launch(share=args.share,server_name="0.0.0.0")
+    demo.launch(share=args.share,server_name="0.0.0.0",server_port=80)
